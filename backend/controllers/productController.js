7692b2b2e2c3b3ceba61946c9d112999af03ce39
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-03 14:10:22 +0530

    added product schema and create and get product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,23 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({message : "Route is working fine"})
+const Product = require("../models/productModel")
+
+
+
+// Create Product
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success:true,
+    product
+  })
+}
+
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find()
+  res.status(200).json({
+    message : "Route is working fine",
+    success:true,
+    products
+  })
 }
