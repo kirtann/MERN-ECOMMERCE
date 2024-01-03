ffb9af04b7a4786fb2c67495898800205761af10
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-03 14:39:20 +0530

    created delete product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,7 +19,6 @@ exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
 
   res.status(200).json({
-    message : "Route is working fine",
     success:true,
     products
   })
@@ -49,4 +48,23 @@ exports.updateProduct = async (req, res, next) => {
     product
   })
 
+}
+
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id)
+
+  if(!product){
+    return res.status(500).json({
+      success:false,
+      message:"Product not found"
+    })
+  }
+
+  await Product.deleteOne(product);
+
+  res.status(200).json({
+    success:true,
+    message:"Product Deteted successfully"
+  })
 }
