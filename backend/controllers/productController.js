e57b08a0f68dfdc0f8cbfdb5606df1b105f11b8e
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-03 14:28:07 +0530

    created update product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require("../models/productModel")
 
 
 
-// Create Product
+// Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -10,14 +10,43 @@ exports.createProduct = async (req, res, next) => {
     success:true,
     product
   })
+
 }
 
+// Get All Product
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find()
+  const products = await Product.find();
+
   res.status(200).json({
     message : "Route is working fine",
     success:true,
     products
   })
+
+}
+
+// Update Product -- Admin
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if(!product){
+    return res.status(500).json({
+      success:false,
+      message:"Product not found"
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false
+  });
+
+  res.status(200).json({
+    success:true,
+    product
+  })
+
 }
