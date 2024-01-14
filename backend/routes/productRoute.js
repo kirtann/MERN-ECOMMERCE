45201ac3d72a5b04d4461283d39c26e70198fe10
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 16:48:59 +0530

    added get single product details api request route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.route("/product/new").post(createProduct)
 
 router.route("/product/:id").put(updateProduct)
 
-router.route("/product/:id").delete(deleteProduct)
+router.route("/product/:id").delete(deleteProduct).get(getProductDetails)
 
 module.exports = router
