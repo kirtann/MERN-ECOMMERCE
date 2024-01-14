e685195208889c7e36250b78dfde24c46f214552
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 17:13:22 +0530

    added error handling class

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/productModel")
-
+const ErrorHandler = require("../utils/errorHandler")
 
 
 // Create Product -- Admin
@@ -31,10 +31,7 @@ exports.getProductDetails =async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success:false,
-      message:"Product not found"
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   res.status(200).json({
