c3983eeb4f710de567b333d8b0b98dc88d9c48a8
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 17:24:41 +0530

    solved unhandled promise rejection error

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const connectDatabase = ()=>{
   mongoose.connect(process.env.DB_URI).then((data)=>{
     console.log(`connected to mongodb ith server: ${data.connection.host}`);
-  }).catch((err)=>{
-    console.log(err)
-  })
+  });
 }
 
 module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,19 @@ dotenv.config({path:'backend/config/config.env'})
 // Connecting to database
 connectDatabase()
 
-app.listen(process.env.PORT, ()=>{
+const server = app.listen(process.env.PORT, ()=>{
 
   console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
+
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", err=>{
+  console.log(`Error: ${err.message}`);
+  console.log("shutting dowm the server due to Unhandled Promise Rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
