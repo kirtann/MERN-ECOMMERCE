87889ed727a940fb74ace52e147954045404cba7
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 17:31:48 +0530

    Handling Mongodb id errors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,14 @@ const dotenv = require('dotenv')
 const connectDatabase = require("./config/database")
 const { connect } = require('mongoose')
 
+// Handling UnCaught Exception
+process.on("uncaughtException", err=>{
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Uncaught Exception Error")
+
+  process.exit(1);
+})
+
 // Config
 
 dotenv.config({path:'backend/config/config.env'})
@@ -14,13 +22,12 @@ connectDatabase()
 const server = app.listen(process.env.PORT, ()=>{
 
   console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
-
+});
 
 // Unhandled Promise Rejection
 process.on("unhandledRejection", err=>{
   console.log(`Error: ${err.message}`);
-  console.log("shutting dowm the server due to Unhandled Promise Rejection");
+  console.log("Shutting dowm the server due to Unhandled Promise Rejection");
 
   server.close(() => {
     process.exit(1);
