e8ead9add7503886ad6bb5e6a1be1857b4090313
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 17:19:28 +0530

    added async error handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,9 @@
 const Product = require("../models/productModel")
 const ErrorHandler = require("../utils/errorHandler")
-
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
 
 // Create Product -- Admin
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors( async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
@@ -11,23 +11,22 @@ exports.createProduct = async (req, res, next) => {
     product
   })
 
-}
+});
 
 // Get All Product
 
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
 
   res.status(200).json({
     success:true,
     products
   })
-
-}
+});
 
 // Get Product Details
 
-exports.getProductDetails =async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -38,11 +37,11 @@ exports.getProductDetails =async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // Update Product -- Admin
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if(!product){
@@ -63,10 +62,10 @@ exports.updateProduct = async (req, res, next) => {
     product
   })
 
-}
+});
 
 // Delete Product
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id)
 
   if(!product){
@@ -82,5 +81,5 @@ exports.deleteProduct = async (req, res, next) => {
     success:true,
     message:"Product Deteted successfully"
   })
-}
+});
 
