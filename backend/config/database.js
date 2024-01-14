58c666fdb1eea1db2e7915c97f9bea428756fb5c
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-14 16:35:11 +0530

    updated database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-
-dotenv.config({path: 'config.env'})
 
 const connectDatabase = ()=>{
   mongoose.connect(process.env.DB_URI).then((data)=>{
