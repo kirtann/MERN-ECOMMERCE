33c101e380f1bddd4221064082937b597fb51e46
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-17 12:55:01 +0530

    added product filtering feature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,9 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeatures = new ApiFeatures(Product.find(), req.query).search();
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
   const products = await apiFeatures.query;
 
   res.status(200).json({
