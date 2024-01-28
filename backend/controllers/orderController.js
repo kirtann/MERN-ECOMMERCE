c7cea798c01bfd7820c36ab0265e7e7a09152d3f
kirtann <94744255+kirtann@users.noreply.github.com>
2024-01-28 14:43:58 +0530

    get order details and my order route created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,30 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// Get Single Order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    succes: true,
+    order,
+  });
+});
+
+// Get logged in user Orders
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    succes: true,
+    orders,
+  });
+});
